735623e4d50ab664e457e68a7ed73605a7b5cf5a
Pratik Narendra Gupta <pratikngupta@outlook.com>
2024-03-08 08:41:29 -0500

    chore: submitting the assignment

--- pgupta85-SE3314bassignment1/Client/test-error.sh
+++ pgupta85-SE3314bassignment1/Client/test-error.sh
@@ -18,11 +18,6 @@ testNumber=0
 # Create an array with the image names
 images=("BleedingHeart.pdf" "Canna.gif" "cow.png")
 
-# get 2nd element of the array
-echo -e "\033[0;32mTest Number: $testNumber"
-echo -e "Test: Getting ${images[0]}: inncorrect file extension\033[0m,"
-
-
 let "testNumber++"
 echo -e "\033[0;32mTest Number: $testNumber"
 echo -e "Test: Getting ${images[0]}: incorrect file extension\033[0m,"
@@ -32,24 +27,23 @@ echo ""
 
 let "testNumber++"
 echo -e "\033[0;32mTest Number: $testNumber"
-echo -e "Test: Getting ${images[0]}: inncorrect Packet\033[0m,"
-echo -e "\033[0;34mCommand to run:\033[0m node GetImage -s $SERVER -q ${images[0]} -v 9"
-node GetImage -s $SERVER -q ${images[0]} -v 9
+echo -e "Test: Getting ${images[1]}: inncorrect Packet\033[0m,"
+echo -e "\033[0;34mCommand to run:\033[0m node GetImage -s $SERVER -q ${images[1]} -v 10"
+node GetImage -s $SERVER -q ${images[1]} -v 10
 echo ""
 
 let "testNumber++"
 echo -e "\033[0;32mTest Number: $testNumber"
-echo -e "Test: Getting ${images[0]}: File Not found\033[0m,"
-echo -e "\033[0;34mCommand to run:\033[0m node GetImage -s $SERVER -q ${images[0]} -v 9"
-node GetImage -s $SERVER -q ${images[0]} -v 9
+echo -e "Test: Getting ${images[2]}: File Not found\033[0m,"
+echo -e "\033[0;34mCommand to run:\033[0m node GetImage -s $SERVER -q ${images[2]} -v 9"
+node GetImage -s $SERVER -q ${images[2]} -v 9
 echo ""
 
 let "testNumber++"
 echo -e "\033[0;32mTest Number: $testNumber"
-echo -e "Test: Getting ${images[0]}: Wrong arrgument\033[0m,"
+echo -e "Test: Getting ${images[1]}: Wrong arrgument\033[0m,"
 echo -e "\033[0;34mCommand to run:\033[0m node GetImage"
 node GetImage
 echo ""
 
 exit 0
-
